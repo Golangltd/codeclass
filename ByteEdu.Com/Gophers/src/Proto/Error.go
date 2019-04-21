e471c654f1f3b476fcec5c50e8112924ffb1e4c8
cserli <1528884535@qq.com>
2019-04-21 23:58:30 +0800

    gophers

---
+++ ByteEdu.Com/Gophers/src/Proto/Error.go
@@ -0,0 +1,3 @@
+package Proto
+
+// 错误码
--- ByteEdu.Com/Gophers/src/Proto/Proto.go
+++ ByteEdu.Com/Gophers/src/Proto/Proto.go
@@ -9,6 +9,12 @@ const (
 
 )
 
+// 排行的结构
+type STRank struct {
+	Rank      int
+	LoginName string
+}
+
 //------------------------------------------------------------------------------
 
 /*
@@ -18,7 +24,27 @@ const (
 	Proto2INIT            = iota // 初始化
 	C2S_PlayerLoginProto2        // C2S_PlayerLoginProto2 == 1  用户登录协议
 	S2C_PlayerLoginProto2        // S2C_PlayerLoginProto2 == 2
+
+	C2S_GetRankProto2 // C2S_GetRankProto2 == 3  主动拉取排行
+	S2C_GetRankProto2 // S2C_GetRankProto2 == 4
+
 )
+
+//------------------------------------------------------------------------------
+// 主动拉取排行
+type C2S_GetRank struct {
+	Protocol  int
+	Protocol2 int
+	LoginName string
+	Score     int
+	Token     string
+}
+
+type S2C_GetRank struct {
+	Protocol  int
+	Protocol2 int
+	MapRank   map[int]*STRank // 取前10名
+}
 
 //------------------------------------------------------------------------------
 // 用户登录或者注册
@@ -34,6 +60,7 @@ type S2C_PlayerLogin struct {
 	Protocol  int
 	Protocol2 int
 	Token     string
+	OpenID    string
 }
 
 //------------------------------------------------------------------------------
