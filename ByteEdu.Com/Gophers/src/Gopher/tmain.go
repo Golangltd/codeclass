fa7145aef9c9fb8324f255a1da2240a46eac756b
cserli <1528884535@qq.com>
2019-04-25 18:36:18 +0800

    Update tmain.go

--- ByteEdu.Com/Gophers/src/Gopher/tmain.go
+++ ByteEdu.Com/Gophers/src/Gopher/tmain.go
@@ -9,9 +9,27 @@ import (
 
 // http://127.0.0.1:8891/ByteEdu_Gophers?Protocol=1&Protocol2=1
 
+//------------------------------------------------------------------------------
+// 保存玩家登陆、注册的协议
+type PlayerData struct {
+	OpenID    string
+	LoginName string
+	LoginPW   string
+	Lev       string // 职位
+}
+
+//------------------------------------------------------------------------------
+
 var (
-	strport string = "8891"
+	strport    string = "8891"
+	PlayerData map[string]*PlayerData
 )
+
+func init() {
+	// 初始化 map
+	PlayerData = make(map[string]*PlayerData)
+	return
+}
 
 func main() {
 
@@ -24,10 +42,11 @@ func IndexHandler(w http.ResponseWriter, req *http.Request) {
 	if req.Method == "GET" {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		req.ParseForm()
-		defer func() { // 必须要先声明defer，否则不能捕获到panic异常
+		defer func() {
 			if err := recover(); err != nil {
 				fmt.Println("%s", err)
 				req.Body.Close()
+				return
 			}
 		}()
 
@@ -35,9 +54,7 @@ func IndexHandler(w http.ResponseWriter, req *http.Request) {
 		Protocol2, bProtocol2 := req.Form["Protocol2"]
 
 		if bProtocol && bProtocol2 {
-			// 主协议判断
 			if Protocol[0] == strconv.Itoa(Proto.ProtoGopher) {
-				// 子协议判断
 				switch Protocol2[0] {
 				case strconv.Itoa(Proto.C2S_PlayerLoginProto2):
 					// data := DB_rpc_()
@@ -47,7 +64,6 @@ func IndexHandler(w http.ResponseWriter, req *http.Request) {
 					//------------------------------------------------------
 					return
 				case strconv.Itoa(Proto.C2S_GetRankProto2):
-					// 获取排行
 					return
 				default:
 					fmt.Fprintln(w, "88902")
@@ -57,7 +73,6 @@ func IndexHandler(w http.ResponseWriter, req *http.Request) {
 			fmt.Fprintln(w, "88904")
 			return
 		}
-		// 服务器获取通信方式错误 --> 8890 + 1
 		fmt.Fprintln(w, "88901")
 		return
 	}
