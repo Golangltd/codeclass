9490dab99e0220208df109c2c9db0c498f411a85
cserli <1528884535@qq.com>
2019-04-26 00:06:26 +0800

    Update tmain.go

--- ByteEdu.Com/Gophers/src/Gopher/tmain.go
+++ ByteEdu.Com/Gophers/src/Gopher/tmain.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 )
 
-// http://127.0.0.1:8891/ByteEdu_Gophers?Protocol=1&Protocol2=1
+// http://127.0.0.1:8891/ByteEdu_Gophers?Protocol=1&Protocol2=1&Itype=1&LoginName=ByteEdu.Com&LoginPW=ByteEdu.Com
 
 //------------------------------------------------------------------------------
 // 保存玩家登陆、注册的协议
@@ -21,13 +21,13 @@ type PlayerData struct {
 //------------------------------------------------------------------------------
 
 var (
-	strport    string = "8891"
-	PlayerData map[string]*PlayerData
+	strport     string = "8891"
+	PlayerDataG map[string]*PlayerData
 )
 
 func init() {
 	// 初始化 map
-	PlayerData = make(map[string]*PlayerData)
+	PlayerDataG = make(map[string]*PlayerData)
 	return
 }
 
@@ -52,15 +52,42 @@ func IndexHandler(w http.ResponseWriter, req *http.Request) {
 
 		Protocol, bProtocol := req.Form["Protocol"]
 		Protocol2, bProtocol2 := req.Form["Protocol2"]
+		Itype, bItype := req.Form["Itype"]
 
-		if bProtocol && bProtocol2 {
+		if bProtocol && bProtocol2 && bItype {
 			if Protocol[0] == strconv.Itoa(Proto.ProtoGopher) {
 				switch Protocol2[0] {
 				case strconv.Itoa(Proto.C2S_PlayerLoginProto2):
-					// data := DB_rpc_()
-					// b, _ := json.Marshal(data)
-					// fmt.Fprint(w, base64.StdEncoding.EncodeToString(b))
-					fmt.Fprintln(w, "登陆成功")
+
+					LoginName, _ := req.Form["LoginName"]
+					LoginPW, _ := req.Form["LoginPW"]
+					strItype := Itype[0]
+					if strItype == "1" { // 注册
+						//1. 用户名要脏字过滤
+						//2. 判断用户是否注册过
+						_, ok := PlayerDataG[LoginName[0]]
+						if ok {
+							fmt.Fprintln(w, "已经注册过！")
+							return
+						} else {
+							data := &PlayerData{
+								OpenID:    LoginName[0],
+								LoginName: LoginName[0],
+								LoginPW:   LoginPW[0],
+								Lev:       "军师", // 职位
+							}
+							// 保存
+							PlayerDataG[LoginName[0]] = data
+							fmt.Fprintln(w, "注册成功")
+							return
+						}
+						return
+					} else if strItype == "2" { // 登陆
+
+						return
+					}
+
+					fmt.Fprintln(w, "类型错误")
 					//------------------------------------------------------
 					return
 				case strconv.Itoa(Proto.C2S_GetRankProto2):
