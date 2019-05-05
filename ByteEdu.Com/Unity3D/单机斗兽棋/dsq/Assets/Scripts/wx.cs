632156445540e44276ab0641a7952901bd36cb6e
cserli <1528884535@qq.com>
2019-05-05 19:56:12 +0800

    dsq

---
+++ ByteEdu.Com/Unity3D/单机斗兽棋/dsq/Assets/Scripts/wx.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wx : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /*
+        微信接入
+     */
+    
+    public void WX_TiaoZhuan()
+    {
+        Application.LoadLevel(1);
+    }
+ 
+}
